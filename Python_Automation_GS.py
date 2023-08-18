aba0e276be1667df8a21d359b1b6309ce1d9582f
praveenmore <41004966+praveenmore@users.noreply.github.com>
2023-08-18 16:57:29 +0530

    Update Python_Automation_GS.py

--- Python_Automation_GS.py
+++ Python_Automation_GS.py
@@ -1,36 +1,61 @@
+import yfinance as yf
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+import matplotlib.pyplot as plt
 
-# Load credentials from the downloaded JSON file
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("python-automation-program-gs-89544358d26f.json", scope)
-client = gspread.authorize(creds)
+# Replace with your top 20 stock symbols
+top_stocks = ["AAPL", "GOOGL", "AMZN", "MSFT", "TSLA", "NVDA", "NFLX", "PYPL", "AMGN", "ADBE", "COST", "INTC",
+              "CMCSA", "PEP", "AVGO", "CSCO", "TXN", "QCOM", "CHTR"]
 
-# Open the Google Sheet by its title
-sheet = client.open("Stock Data and Graphs")
+# Fetch hourly data using yfinance
+def fetch_stock_data(symbol):
+    stock = yf.Ticker(symbol)
+    data = stock.history(period="1d", interval="1h")
+    return data
 
-# Access a specific worksheet by title or index
-worksheet = sheet.get_worksheet(0)  # Replace with your worksheet's index or title
+# Authenticate and write data to Google Sheets
+def write_to_google_sheets(data, symbol):
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("python-automation-program-gs-89544358d26f.json", scope)
+    client = gspread.authorize(creds)
 
-# Write data to the sheet
-data = [["Timestamp", "Stock A", "Stock B", ...],  # Header row
-        ["2023-01-01 14:00:00", 100.00, 150.00, ...],  # Example data
-        # ... Add more rows of data ...
-       ]
-worksheet.insert_rows(data, row=1)  # Insert data starting from the first row
+    sheet = client.open("Stock Data and Graphs")
+    worksheet = sheet.get_worksheet(0)
 
-# Close the worksheet
-worksheet.update_acell("A1", "Last Updated: " + str(datetime.now()))
+    next_row = len(worksheet.get_all_values()) + 1
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    values = [timestamp, symbol] + data["Close"].tolist()
+    worksheet.insert_row(values, next_row)
 
-# # Access a different worksheet for the graph (sheet2)
-# graph_worksheet = sheet.add_worksheet(title="Graph Data", rows="100", cols="20")
-#
-# # Write graph data to the new worksheet
-# graph_data = [["Timestamp", "Stock A Rise/Fall", "Stock B Rise/Fall", ...],  # Header row
-#               ["2023-01-01 14:00:00", "+5.00%", "-2.50%", ...],  # Example data
-#               # ... Add more rows of data ...
-#              ]
-# graph_worksheet.insert_rows(graph_data, row=1)
-#
-# # Done!
+# Plot stock price graph and write to Google Sheets
+def plot_and_write_graph(data, symbol):
+    plt.figure(figsize=(10, 6))
+    data.plot()
+    plt.title(f"{symbol} Stock Price Movement")
+    plt.xlabel("Time")
+    plt.ylabel("Price")
+    plt.grid(True)
+
+    image_path = f"{symbol}_plot.png"
+    plt.savefig(image_path)
+    plt.close()
+
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("python-automation-program-gs-89544358d26f.json", scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open("Stock Data and Graphs")
+    worksheet = sheet.add_worksheet(title=symbol, rows="100", cols="20")
+
+    worksheet.update("A1", f"={image_path}")
+
+# Main function
+def main():
+    for symbol in top_stocks:
+        data = fetch_stock_data(symbol)
+        write_to_google_sheets(data, symbol)
+        plot_and_write_graph(data, symbol)
+
+if __name__ == "__main__":
+    main()
